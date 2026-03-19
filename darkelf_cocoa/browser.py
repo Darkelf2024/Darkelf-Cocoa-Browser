22cb266486c1872cc1c5ec57734be3b24b171ac0
Darkelf2024 <174353864+Darkelf2024@users.noreply.github.com>
2026-03-19 03:20:41 -0500

    Update version to v4.0.14 in browser.py

--- darkelf_cocoa/browser.py
+++ darkelf_cocoa/browser.py
@@ -1,4 +1,4 @@
-# Darkelf Cocoa General Browser v4.0.13 — Ephemeral, Privacy-Focused Web Browser (macOS / Cocoa Build)
+# Darkelf Cocoa General Browser v4.0.14 — Ephemeral, Privacy-Focused Web Browser (macOS / Cocoa Build)
 # Copyright (C) 2025 Dr. Kevin Moore
 #
 # SPDX-License-Identifier: LGPL-3.0-or-later
